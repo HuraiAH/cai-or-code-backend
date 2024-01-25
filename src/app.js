0c0d38799513de981dee73d879eb83a59b5c782a
HuraiAH <huraiah17@gmail.com>
2024-01-25 23:57:51 +0600

    create user model

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
-const cookieParser = require("cookie-parser");
 const express = require("express");
 const app = express();
-// uses express custom middleware
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+// uses express custom middleware
+app.use(
+   cors({
+      origin: process.env.CORS_ORIGIN,
+      credentials: true,
+   })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
