8fe7c43f3f5e7a636e8508839b40ce75d2c6f135
HuraiAH <huraiah17@gmail.com>
2024-01-25 12:19:01 +0600

    added express custom middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,11 @@
+// const cookieParser= require("express").cookieParser()
 const express = require("express");
 const app = express();
+// uses express custom middleware
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+// app.use(cookieParser)
 
 module.exports = app;
