3788e584bd59ead17a1215874c2b974691934401
HuraiAH <huraiah17@gmail.com>
2024-01-27 11:20:46 +0600

    create user and video model with middleware

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 const { Schema, model, mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
+const JWT = require("jsonwebtoken");
 // define user schema
 let userSchema = new Schema(
    {
@@ -17,11 +19,13 @@ let userSchema = new Schema(
       },
       email: {
          type: String,
-         required: [true, "email is required!"],
+         required: [true, "Email is required!"],
          unique: true,
          lowercase: true,
          trim: true,
+         match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
       },
+
       avatar: {
          type: String, // cloudinary  url
          required: [true, "avatar is required!"],
@@ -45,6 +49,44 @@ let userSchema = new Schema(
    },
    { timestamps: true }
 );
+// password pre save middleware
+userSchema.pre("save", async function (next) {
+   if (this.isModified("password")) {
+      this.password = await bcrypt.hash(this.password, 8);
+   }
+   next();
+});
+// Add a method to compare passwords
+userSchema.methods.comparePassword = async function (candidatePassword) {
+   try {
+      return await bcrypt.compare(candidatePassword, this.password);
+   } catch (error) {
+      throw error;
+   }
+};
+// generate access token
+userSchema.methods.generateAccessToken = function () {
+   return JWT.sign(
+      {
+         _id: this._id,
+         UserName: this.UserName,
+         email: this.email,
+      },
+      process.env.Access_token_secret_key,
+      { expiresIn: process.env.Access_token_expiry }
+   );
+};
+// generate refresh token
+userSchema.methods.generateRefreshToken = function () {
+   return JWT.sign(
+      {
+         _id: this._id,
+      },
+      process.env.Refresh_token_secret_key,
+      { expiresIn: process.env.Refresh_token_expiry }
+   );
+};
+
 // create new model
 const User = model("User", userSchema);
 // export model
